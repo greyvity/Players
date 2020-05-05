82f9c612742a51efc2f6e0b8c77ea33bdb56c078
yogesh123098 <yogeshpant293@gmail.com>
2020-05-05 12:34:52 +0545

    Updates

---
+++ precache-manifest.c0a2dd727ea17e7720ae8fdbf482d2c7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a98fefbc565f5261cc29d606433bff",
+    "url": "/Players/index.html"
+  },
+  {
+    "revision": "5f1cb88c309ec469db62",
+    "url": "/Players/static/css/main.55f744b3.chunk.css"
+  },
+  {
+    "revision": "69dd91d7c9e721f21c18",
+    "url": "/Players/static/js/2.cf2333cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Players/static/js/2.cf2333cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f1cb88c309ec469db62",
+    "url": "/Players/static/js/main.c49048b3.chunk.js"
+  },
+  {
+    "revision": "328e55fccd86f751c391",
+    "url": "/Players/static/js/runtime-main.2ad6c1bf.js"
+  },
+  {
+    "revision": "8d647f68d81408dde5c4e1ed7dfb8527",
+    "url": "/Players/static/media/Begin.8d647f68.mp3"
+  },
+  {
+    "revision": "e175412e95b9aeba27a71b47937e0fad",
+    "url": "/Players/static/media/creative.e175412e.mp3"
+  },
+  {
+    "revision": "0466433ce33c4df06743075bef864477",
+    "url": "/Players/static/media/cute.0466433c.mp3"
+  },
+  {
+    "revision": "80276ac2e9b80cdde919b3c40b4a79cc",
+    "url": "/Players/static/media/rock.80276ac2.mp3"
+  },
+  {
+    "revision": "fd79b1142f66f1981d0aea1a0b1501ea",
+    "url": "/Players/static/media/summer.fd79b114.mp3"
+  },
+  {
+    "revision": "9569869286a4e88efa8724d24f50bf47",
+    "url": "/Players/static/media/ukelele.95698692.mp3"
+  }
+]);
