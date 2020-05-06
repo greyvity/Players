a16b9b590db8028fd4e9a6f7c3da39f1a332474d
yogesh123098 <yogeshpant293@gmail.com>
2020-05-06 17:18:44 +0545

    BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import ControlAudio from "./Components/Audio/ControlAudio/ControlAudio";
 import ControlVideo from "./Components/Video/ControlVideo/ControlVideo";
